c55181d33d782e2f514d8cd0bd9cc789f054caaf
Chaganti1308 <93071195+Chaganti1308@users.noreply.github.com>
2024-02-08 21:46:20 +0530

    Update operators.js

--- JavaScript/operators.js
+++ JavaScript/operators.js
@@ -74,3 +74,80 @@ console.log(3>4 && 4<5);
 console.log(3<4 || 4<5);
 console.log(3>4 || 4<5);
 console.log(3>4 || 4>5);
+
+// Assignment
+/** 
+var first_name = "Neeharika";
+
+// Arithmetic
+// +,-,*,/,%,**,++,__
+console.log(4+5);
+
+var a = 20;
+var b = 9;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(4**3);
+
+*/
+
+// post increment
+
+// var i = 1;
+// console.log(i++);
+// console.log(i);
+
+// pre increment
+
+// var i = 12;
+// console.log(++i);
+// console.log(i);
+
+
+//Relational 
+
+// equality check
+// console.log(1==2); // false
+// console.log(1==1); // false
+
+// strict equality 
+
+// console.log(1==='1');  // false
+// console.log(1===1); // true
+
+/** 
+ * in equality check
+ */
+
+// console.log(1 !='1');  // false
+// console.log(1 !== "1"); // true
+
+
+/** 
+ * Comparative
+ */
+
+// console.log(3>4);
+// console.log(3<4);
+// console.log(3<=4);
+// console.log(3>=4);
+// console.log(3==3);
+// console.log(3!=3);
+
+/**
+ * Logical
+ */
+// && -- and, if both conditions are true, result will be true
+// || -- or, if any of the condition is true , result will be true
+// ^ -- NOT
+
+console.log(3<4 && 4<5);
+console.log(3>4 && 4<5);
+
+console.log(3<4 || 4<5);
+console.log(3>4 || 4<5);
+console.log(3>4 || 4>5);
